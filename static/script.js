33d08afbc6181bc9830af9d4f2019646cbe13e90
Markus Strasser <strasser.ms@gmail.com>
2023-04-05 12:00:13 -0400

    styling

--- static/script.js
+++ static/script.js
@@ -4,6 +4,16 @@ document.addEventListener("DOMContentLoaded", () => {
   const tokenCountDisplay = document.getElementById("token-count");
   const tokenStringsDisplay = document.getElementById("token-strings");
 
+  const backgroundColors = ["#FFE432", "#FFCB25", "#FFAE17", "#FFC17C", "#FFA500", "#FF8C00"];
+
+   // Submit the form when pressing the ENTER key
+  textInput.addEventListener("keydown", (event) => {
+    if (event.key === "Enter") {
+      event.preventDefault();
+      form.dispatchEvent(new Event("submit"));
+    }
+  });
+  
   form.addEventListener("submit", async (event) => {
     event.preventDefault();
 
@@ -22,14 +32,33 @@ document.addEventListener("DOMContentLoaded", () => {
       // Clear the previous tokens
       tokenStringsDisplay.innerHTML = "";
 
-      // Create a new span element for each token and append it to the container
-      data.token_strings.forEach((token) => {
+      // Initialize the background color index
+      let bgColorIndex = 0;
+
+      data.token_strings.forEach((token, index) => {
         const span = document.createElement("span");
         span.textContent = token;
-        span.style.backgroundColor = "yellow";
         span.style.padding = "4px";
         span.style.marginRight = "4px";
-        span.style.borderRadius = "4px";
+        span.style.marginLeft = "4px";
+
+        span.style.borderRadius = "1px";
+
+        // Update the background color index if the token is part of the same word as the previous token
+        if (index > 0 && !token.startsWith(" ") && !data.token_strings[index - 1].endsWith(" ")) {
+          bgColorIndex = (bgColorIndex + 1) % backgroundColors.length;
+          span.style.marginRight = "0px";
+          span.style.marginLeft = "0px";
+
+
+        } else {
+          bgColorIndex = 0;
+          // span.style.marginRight = "7px";
+
+        }
+
+        span.style.backgroundColor = backgroundColors[bgColorIndex];
+       
         tokenStringsDisplay.appendChild(span);
       });
     } else {
